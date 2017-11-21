9376a8ab6a708004a349794a008bc328a29c4015
mbonnefoy <matthieu.bonnefoy@gmail.com>
2017-11-22 08:06:58 +1300

    Add a navigation bar

--- frontend/App.js
+++ frontend/App.js
@@ -1,23 +1,37 @@
 import React from 'react';
 import { TabBarIOS, Text, View } from 'react-native';
+import NavigationBar from 'react-native-navbar';
+
+const styles = {
+  container: {
+    flex: 1,
+  },
+};
 
 export default class App extends React.Component {
   render() {
     return (
-      <TabBarIOS>
-        <TabBarIOS.Item
-          title="Wish List"
-          icon={require('./img/list.png')}>
-        </TabBarIOS.Item>
-        <TabBarIOS.Item
-          title="Reading"
-          icon={require('./img/reading.png')}>
-        </TabBarIOS.Item>
-        <TabBarIOS.Item
-          title="Bookshelf"
-          icon={require('./img/shelf.png')}>
-        </TabBarIOS.Item>
-      </TabBarIOS>
+      <View style={styles.container}>
+        <NavigationBar
+          style={{borderBottomWidth: 0.3, borderBottomColor: 'gray'}}
+          title={{title: 'Reading List'}}
+          tintColor="#F5F5F5"
+        />
+        <TabBarIOS>
+          <TabBarIOS.Item
+            title="Wish List"
+            icon={require('./img/list.png')}>
+          </TabBarIOS.Item>
+          <TabBarIOS.Item
+            title="Reading"
+            icon={require('./img/reading.png')}>
+          </TabBarIOS.Item>
+          <TabBarIOS.Item
+            title="Bookshelf"
+            icon={require('./img/shelf.png')}>
+          </TabBarIOS.Item>
+        </TabBarIOS>
+      </View>
     );
   }
 }
