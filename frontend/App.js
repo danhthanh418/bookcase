62b1bd7956a676280844e0be278e4d06291631dc
mbonnefoy <matthieu.bonnefoy@gmail.com>
2017-11-23 08:00:24 +1300

    Add tab bar navigation

--- frontend/App.js
+++ frontend/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { TabBarIOS, Text, View } from 'react-native';
 import NavigationBar from 'react-native-navbar';
 
+
 const styles = {
   container: {
     flex: 1,
@@ -9,26 +10,54 @@ const styles = {
 };
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: 'reading'
+    };
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <NavigationBar
-          style={{borderBottomWidth: 0.3, borderBottomColor: 'gray'}}
-          title={{title: 'Reading List'}}
+          style={{borderBottomWidth: 0.3, borderBottomColor: "gray"}}
+          title={{title: "Reading List"}}
           tintColor="#F5F5F5"
         />
-        <TabBarIOS>
+        <TabBarIOS selectedTab={this.state.selectedTab}>
           <TabBarIOS.Item
+            selected={this.state.selectedTab === "list"}
             title="Wish List"
-            icon={require('./img/list.png')}>
+            icon={require("./img/list.png")}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'list'
+              });
+            }}>
+          <Text>TODO: Wish list view</Text>
           </TabBarIOS.Item>
           <TabBarIOS.Item
+            selected={this.state.selectedTab === "reading"}
             title="Reading"
-            icon={require('./img/reading.png')}>
+            icon={require("./img/reading.png")}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'reading'
+              });
+            }}>
+          <Text>TODO: Reading view</Text>
           </TabBarIOS.Item>
           <TabBarIOS.Item
+            selected={this.state.selectedTab === "shelf"}
             title="Bookshelf"
-            icon={require('./img/shelf.png')}>
+            icon={require("./img/shelf.png")}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'shelf'
+              });
+            }}>
+          <Text>TODO: Bookshelf view</Text>
           </TabBarIOS.Item>
         </TabBarIOS>
       </View>
