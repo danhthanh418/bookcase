a5ac92e489b49ba91edd09dff27731d8e3ee574c
mbonnefoy <matthieu.bonnefoy@gmail.com>
2017-11-25 22:05:06 +1300

    Add django cors headers locally

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,3 +1,14 @@
 from .base import *
 
 DEBUG = True
+
+INSTALLED_APPS += (
+    'corsheaders',
+)
+
+MIDDLEWARE += (
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+)
+
+CORS_ORIGIN_ALLOW_ALL = True
