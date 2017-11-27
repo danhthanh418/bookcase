aaf65cc4492f97a845b80e8427764ab855a1fc09
mbonnefoy <matthieu.bonnefoy@gmail.com>
2017-11-27 22:15:08 +1300

    Use data fetched from api
    We use an external API for testing

--- frontend/App.js
+++ frontend/App.js
@@ -9,20 +9,19 @@ const styles = {
   },
 };
 
-const data = [
-  { id: 1, title: '1' },
-  { id: 2, title: '2' },
-  { id: 3, title: '3' },
-  { id: 4, title: '4' },
-  { id: 5, title: '5' },
-  { id: 6, title: '6' },
-  { id: 7, title: '7' },
-  { id: 8, title: '8' },
-  { id: 9, title: '9' },
-  { id: 10, title: '10' },
-  { id: 11, title: '11' },
-  { id: 12, title: '12' },
-];
+// FIXME: replace by appropriate url
+const url = "https://jsonplaceholder.typicode.com/users";
+var data = [];
+fetch(url)
+  .then(response => response.json())
+  .then(json => {
+      json.forEach((user) => {
+          data.push({
+              id: user.id,
+              title: user.username,
+          });
+      });
+  });
 
 const leftButtonConfig = {
   title: "Add",
