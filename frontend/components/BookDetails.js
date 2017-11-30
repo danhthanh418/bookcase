d90a80224e5a2453725c7330a357bcb7bf88529e
mbonnefoy <matthieu.bonnefoy@gmail.com>
2017-11-30 21:50:32 +1300

    Add text input to book details component

--- frontend/components/BookDetails.js
+++ frontend/components/BookDetails.js
@@ -1,9 +1,50 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, TextInput, View } from 'react-native';
 
 
-const BookDetails = props => (
-  <Text>Item title: {props.navigation.state.params.title}</Text>
-);
+const constants = {
+  NOTES_PLACEHOLDER: 'Type your notes here',
+};
 
-export default BookDetails;
+export default class BookDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: constants.NOTES_PLACEHOLDER };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text
+          style={{
+            paddingLeft: 15,
+            paddingTop: 10,
+            paddingBottom: 10,
+            color: 'gray',
+          }}
+        >
+          NOTES
+        </Text>
+        <TextInput
+          style={{
+            height: 200,
+            backgroundColor: 'white',
+            borderColor: '#C7C7C7',
+            borderWidth: 0.5,
+            fontSize: 16,
+          }}
+          onFocus={() => {
+              if (this.state.text === constants.NOTES_PLACEHOLDER) {
+                this.setState({ text: '' });
+              }
+            }
+          }
+          onChangeText={text => this.setState({ text })}
+          value={this.state.text}
+          multiline
+          numberOfLines={11}
+        />
+      </View>
+    );
+  }
+}
