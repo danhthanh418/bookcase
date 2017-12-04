a5bd97640ea470b3f742b40c9d41bac3328304dc
mbonnefoy <matthieu.bonnefoy@gmail.com>
2017-12-04 21:23:49 +1300

    Refactor bookslist

--- frontend/components/BooksList.js
+++ frontend/components/BooksList.js
@@ -9,56 +9,78 @@ const styles = {
   },
 };
 
-// FIXME: replace by appropriate url (based on tab)
-const url = 'https://jsonplaceholder.typicode.com/albums';
-var data = [];
-fetch(url)
-  .then(response => response.json())
-  .then(json => {
-    json.forEach((item) => {
-      data.push({
-        id: item.id,
-        title: item.title,
+export default class BooksList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+      data: [],
+      error: null,
+      refreshing: false
+    };
+  }
+
+  componentDidMount() {
+    this.makeRemoteRequest();
+  }
+
+  makeRemoteRequest = () => {
+    const url = 'https://jsonplaceholder.typicode.com/albums';
+    this.setState({ loading: true });
+
+    fetch(url)
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          data: json,
+          error: json.error || null,
+          loading: false,
+          refreshing: false
+        });
+      })
+      .catch(error => {
+        this.setState({ error, loading: false });
       });
-    });
-  });
+  };
 
-const BooksList = props => (
-  <View style={styles.container}>
-    <FlatList
-      data={data}
-      keyExtractor={(item, index) => item.id}
-      renderItem={({ item }) => (
-        <ListItem
-          onPress={() => props.navigation.navigate('BookDetails', { title: `${item.title}` })}
-          avatar={
-            <Avatar
-              source={{}}
-              containerStyle={{ marginBottom: 2 }}
-              avatarStyle={{ resizeMode: 'cover' }}
-              width={80}
-              height={120}
+  render() {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={this.state.data}
+          keyExtractor={(item, index) => item.id}
+          renderItem={({ item }) => (
+            <ListItem
+              onPress={() => this.props.navigation.navigate('BookDetails', { title: `${item.title}` })}
+              avatar={
+                <Avatar
+                  source={{}}
+                  containerStyle={{ marginBottom: 2 }}
+                  avatarStyle={{ resizeMode: 'cover' }}
+                  width={80}
+                  height={120}
+                />
+              }
+              title={`${item.title}`}
+              subtitle="Author Goeshere"
+              titleStyle={{ fontSize: 16 }}
+              containerStyle={{ borderBottomWidth: 0, marginBottom: 20 }}
             />
-          }
-          title={`${item.title}`}
-          subtitle="Author Goeshere"
-          titleStyle={{ fontSize: 16 }}
-          containerStyle={{ borderBottomWidth: 0, marginBottom: 20 }}
+          )}
+          ItemSeparatorComponent={() => (
+            <View
+              style={{
+                height: 0.5,
+                width: '95%',
+                backgroundColor: '#CED0CE',
+                marginLeft: '5%',
+                marginBottom: '5%',
+              }}
+            />
+          )}
         />
-      )}
-      ItemSeparatorComponent={() => (
-        <View
-          style={{
-            height: 0.5,
-            width: '95%',
-            backgroundColor: '#CED0CE',
-            marginLeft: '5%',
-            marginBottom: '5%',
-          }}
-        />
-      )}
-    />
-  </View>
-);
-
-export default BooksList;
+      </View>
+    );
+  }
+}
