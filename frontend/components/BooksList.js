9faffadcdd368c5b01619f33ffed0ddfd8b1eb3f
mbonnefoy <matthieu.bonnefoy@gmail.com>
2017-12-08 19:37:21 +1300

    Filter the items based on the tab

--- frontend/components/BooksList.js
+++ frontend/components/BooksList.js
@@ -9,6 +9,13 @@ const styles = {
   },
 };
 
+// maps the tabs and their reading status
+const tabsReadingStatus = {
+  WishListTab: 0,
+  ReadingTab: 1,
+  BookShelfTab: 2
+};
+
 export default class BooksList extends React.Component {
   constructor(props) {
     super(props);
@@ -17,7 +24,8 @@ export default class BooksList extends React.Component {
       loading: false,
       data: [],
       error: null,
-      refreshing: false
+      refreshing: false,
+      readingStatus: tabsReadingStatus[props.navigation.state.routeName],
     };
   }
 
@@ -49,6 +57,16 @@ export default class BooksList extends React.Component {
     const json = [
       {
           "id": 1,
+          "title": "Learning React Native",
+          "authors": [
+              "Bonnie Eisenman"
+          ],
+          "notes": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
+          "rating": null,
+          "reading_status": 0
+      },
+      {
+          "id": 2,
           "title": "Clean Code",
           "authors": [
               "Robert C. Martin",
@@ -59,58 +77,74 @@ export default class BooksList extends React.Component {
           "reading_status": 2
       },
       {
-          "id": 2,
+          "id": 3,
           "title": "Hands-On Machine Learning with Scikit-Learn and TensorFlow",
           "authors": [
               "Aurélien Géron"
           ],
-          "notes": "Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.",
+          "notes": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
           "rating": null,
           "reading_status": 1
-      }
+      },
+      {
+          "id": 4,
+          "title": "Two Scoops of Django 1.11",
+          "authors": [
+              "Audrey Roy Greenfeld",
+              "Daniel Roy Greenfeld"
+          ],
+          "notes": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
+          "rating": null,
+          "reading_status": 2
+      },
     ]
     this.setState({ data: json });
   };
 
+  renderItem = ({item}) => (
+    <ListItem
+      onPress={() => this.props.navigation.navigate('BookDetails', {
+        title: `${item.title}`,
+        notes: `${item.notes}`,
+        readingStatus: `${item.reading_status.toString()}`
+      })}
+      avatar={
+        <Avatar
+          source={{}}
+          containerStyle={{ marginBottom: 2 }}
+          avatarStyle={{ resizeMode: 'cover' }}
+          width={80}
+          height={120}
+        />
+      }
+      title={`${item.title}`}
+      subtitle={`${item.authors}`}
+      titleStyle={{ fontSize: 16 }}
+      containerStyle={{ borderBottomWidth: 0, marginBottom: 20 }}
+    />
+  );
+
+  renderItemSeparatorComponent = () => (
+    <View
+      style={{
+        height: 0.5,
+        width: '95%',
+        backgroundColor: '#CED0CE',
+        marginLeft: '5%',
+        marginBottom: '5%',
+      }}
+    />
+  );
+
   render() {
+    const filteredList = this.state.data.filter(item => item.reading_status === this.state.readingStatus);
     return (
       <View style={styles.container}>
         <FlatList
-          data={this.state.data}
+          data={filteredList}
           keyExtractor={(item, index) => item.id}
-          renderItem={({ item }) => (
-            <ListItem
-              onPress={() => this.props.navigation.navigate('BookDetails', {
-                title: `${item.title}`,
-                notes: `${item.notes}`,
-                readingStatus: `${item.reading_status.toString()}`
-              })}
-              avatar={
-                <Avatar
-                  source={{}}
-                  containerStyle={{ marginBottom: 2 }}
-                  avatarStyle={{ resizeMode: 'cover' }}
-                  width={80}
-                  height={120}
-                />
-              }
-              title={`${item.title}`}
-              subtitle={`${item.authors}`}
-              titleStyle={{ fontSize: 16 }}
-              containerStyle={{ borderBottomWidth: 0, marginBottom: 20 }}
-            />
-          )}
-          ItemSeparatorComponent={() => (
-            <View
-              style={{
-                height: 0.5,
-                width: '95%',
-                backgroundColor: '#CED0CE',
-                marginLeft: '5%',
-                marginBottom: '5%',
-              }}
-            />
-          )}
+          renderItem={this.renderItem}
+          ItemSeparatorComponent={this.renderItemSeparatorComponent}
         />
       </View>
     );
