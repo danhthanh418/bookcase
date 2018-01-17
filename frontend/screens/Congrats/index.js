146f2adcc2c96a58984adb0773c1c3066cda7262
mbonnefoy <matthieu.bonnefoy@gmail.com>
2018-01-18 08:23:26 +1300

    Add minimal styling to congrats screen

--- frontend/screens/Congrats/index.js
+++ frontend/screens/Congrats/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { AsyncStorage, Text, View } from 'react-native';
+import { ActivityIndicator, AsyncStorage, Text, View } from 'react-native';
 import { NavigationActions } from 'react-navigation';
 import { Button } from 'react-native-elements';
+import styles from './styles';
 
-
+/**
+ * Congrats screen displayed on successful book add.
+ */
 export default class Congrats extends React.Component {
   constructor(props) {
     super(props);
@@ -17,7 +20,7 @@ export default class Congrats extends React.Component {
 
   redirectToRoute = async () => {
     try {
-      // this.setState({ loading: true });
+      this.setState({ loading: true });
       let newBook = this.props.navigation.state.params.item;
 
       let books = await AsyncStorage.getItem('@Bookcase:books');
@@ -30,35 +33,51 @@ export default class Congrats extends React.Component {
         const resetAction = NavigationActions.reset({
           index: 0,
           actions: [
-                 {
-                        type: 'Navigation/INIT',
-                        routeName: 'Root',
-                        params: {
-                           filterData: books,
-                        }
-                  }
-             ]
-        })
+            {
+              type: 'Navigation/INIT',
+              routeName: 'Root',
+              params: {
+                filterData: books,
+              }
+            }
+          ]
+        });
 
+        this.setState({ loading: false });
         this.props.navigation.dispatch(resetAction);
       }
     } catch (error) {
-      // this.setState({ error, loading: false });
+      this.setState({ error, loading: false });
       alert(error);
     }
   }
 
-  render() {
+  renderLoading = () => {
+    return <ActivityIndicator size="large" style={[styles.activityIndicatorContainer, styles.horizontal]} color="#00A885" />;
+  };
+
+  renderCongrats = () => {
     return (
-      <View>
-        <Text>Congrats</Text>
+      <View style={styles.container}>
+        <Text style={styles.headline}>Book successfully added!</Text>
         <Button
           onPress={this.redirectToRoute}
           title="Dismiss"
           color="white"
           backgroundColor="#00A885"
+          style={styles.button}
         />
       </View>
     );
+  };
+
+  render() {
+    if (this.state.loading) {
+      return this.renderLoading();
+    } else if (this.state.error) {
+      // return this.renderError();
+    } else {
+      return this.renderCongrats();
+    }
   }
 }
---
+++ frontend/screens/Congrats/styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  headline: {
+    textAlign: 'center',
+    fontSize: 24,
+  },
+  activityIndicatorContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  horizontal: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 10,
+  },
+});
